ed353f55ee13792e6d0b2895d311c2458fe8b97a
Gwilides <83014200+Gwilides@users.noreply.github.com>
2025-10-23 11:27:03 +0200

    feat: Added control of blinking frequency from the button

--- main.c
+++ main.c
@@ -1,25 +1,56 @@
 #include <stdint.h>
 #include <stm32f10x.h>
 
-void delay(uint32_t ticks) {
-	for (int i=0; i<ticks; i++) {
+void delay(uint32_t ticks)
+{
+	for (int i = 0; i < ticks; i++)
+	{
 		__NOP();
 	}
 }
 
-int __attribute((noreturn)) main(void) {
-	// Enable clock for AFIO
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+int __attribute((noreturn)) main(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN;
 
-    while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(1000000);
-	    GPIOC->ODR &= ~(1U<<13U);
-	    delay(1000000);
-    }
+	GPIOA->CRL &= ~(GPIO_CRL_CNF1 | GPIO_CRL_MODE1);
+	GPIOA->CRL |= GPIO_CRL_CNF1_1;
+	GPIOA->ODR |= (1U << 1U);
+
+	GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);
+	GPIOC->CRH |= GPIO_CRH_MODE13_0;
+	GPIOC->ODR |= (1U << 13U);
+
+	uint32_t delays[] = {
+		500000000,
+		250000000,
+		100000000,
+		50000000,
+		25000000};
+	uint8_t mode = 0;
+
+	while (1)
+	{
+		if (!(GPIOA->IDR & GPIO_IDR_IDR1))
+		{
+			if (mode < sizeof(delays) / sizeof(delays[0]) - 1)
+			{
+				mode++;
+			}
+			else
+			{
+				mode = 0;
+			}
+			for (int i = 0; i < 3; i++)
+			{
+				GPIOC->ODR &= ~(1U << 13U);
+				delay(5000000);
+				GPIOC->ODR |= (1U << 13U);
+				delay(5000000);
+			}
+			delay(80000000);
+		}
+		GPIOC->ODR ^= (1U << 13U);
+		delay(delays[mode]);
+	}
 }
